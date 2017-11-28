2b166395834c0eddad9d3df3479520007fba89a6
= <=>
2017-11-28 18:54:54 +0000

    Je n'ai pas avancé

--- tests/Fixture/MoviesFixture.php
+++ tests/Fixture/MoviesFixture.php
@@ -45,9 +45,9 @@ class MoviesFixture extends TestFixture
             'name' => 'Lorem ipsum dolor sit amet',
             'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
             'duration' => 1,
-            'created' => '2017-11-21 18:48:21',
-            'modified' => '2017-11-21 18:48:21',
-            'releasedate' => '2017-11-21 18:48:21'
+            'created' => '2017-11-28 18:49:12',
+            'modified' => '2017-11-28 18:49:12',
+            'releasedate' => '2017-11-28 18:49:12'
         ],
     ];
 }
